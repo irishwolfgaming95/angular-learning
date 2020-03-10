57afe1b61112614245a9b23f2f3a4a77f67d87ce
irishwolfgaming95 <valkyire8@gmail.com>
2020-03-10 13:35:13 -0500

    adding components and modules

--- angular-first/src/app/app.module.ts
+++ angular-first/src/app/app.module.ts
@@ -4,15 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule, 
+    MatMenuModule, MatIconModule, 
+    MatButtonModule, 
+    MatInputModule, 
+    MatCardModule,
+    MatGridListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
